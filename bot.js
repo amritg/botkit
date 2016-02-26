0caf9fe46e8b502dbe6e34fa7bc0c41632c08b92
Amrit Gautam <gautamamrit24@gmail.com>
2016-02-26 11:50:43 +0200

    PlayerId

--- bot.js
+++ bot.js
@@ -83,7 +83,6 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
-var request = require('request');
 
 controller.hears(['record (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
 	var matches = message.text.match(/record (.*)/i);
@@ -95,25 +94,9 @@ controller.hears(['record (.*)'],'direct_message,direct_mention,mention',functio
 		var json = JSON.parse(body);
 		var string = body;
 		var n = string.match(/time":"(.*?)"/i);
-		    
+		  
+		  
 		bot.reply(message,"Record is: " + n[1] + " Seconds");
-		console.log(body)
-	  }
-		catch(err){
-			bot.reply(message,"Not im my Database");
-		}
-	})
-
-   });
-
-controller.hears(['dota (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
-	var matches = message.text.match(/dota (.*)/i);
-    var playerId = matches[1];
-	
-	request('https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?matches_requested=1&account_id=' + playerId+'&key=CED9D559CCCD3B55EBE66DEAAD2983B2', function (error, response, body) {
-	  try{
-		var json = JSON.parse(body);
-		bot.reply(message,"Match Id: " + json.result.matches[0].match_id);
 		console.log(body) // Show the HTML for the Google homepage.
 	  }
 		catch(err){
@@ -121,7 +104,8 @@ controller.hears(['dota (.*)'],'direct_message,direct_mention,mention',function(
 		}
 	})
 
-   });
+    });
+
 
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
